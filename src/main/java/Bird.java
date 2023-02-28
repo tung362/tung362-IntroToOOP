212493d875cb3c92dbbfc6317b00648956ed82d0
Tung Nguyen <7319tnguyen040@gmail.com>
2023-02-28 01:57:53 +0000

    Completed lab

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -44,8 +44,10 @@ public class Bird {
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *         greater than or equal to 3.
      */
-    public String sound(){
-        return "";
+    public String sound()
+    {
+        if (ageMonths < 3) return "Tweet";
+        return "Chirp";
     }
 
 }
